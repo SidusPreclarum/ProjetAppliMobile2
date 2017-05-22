83f6081d280387d428121fff8d73195f6df79f77
Valentin Guilloton <valentin.guilloton@eleve.ensai.fr>
2017-05-22 12:57:53 +0200

    Val 22/05 13h

---
+++ app/src/main/java/com/example/ensai/projetapplimobile/DateActivity.java
@@ -0,0 +1,17 @@
+package com.example.ensai.projetapplimobile;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+/**
+ * Created by ensai on 22/05/17.
+ */
+
+public class DateActivity extends AppCompatActivity {
+
+    public
+
+
+}
--- app/src/main/java/com/example/ensai/projetapplimobile/MainActivity.java
+++ app/src/main/java/com/example/ensai/projetapplimobile/MainActivity.java
@@ -2,12 +2,25 @@ package com.example.ensai.projetapplimobile;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button cle;
+    private Button date;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button cle = (Button) findViewById(R.id.buttonTag);
+        cle.setOnClickListener((View.OnClickListener) this);
+    }
+
+
+    @Override
+    public void onClick(View v) {
+
     }
 }
